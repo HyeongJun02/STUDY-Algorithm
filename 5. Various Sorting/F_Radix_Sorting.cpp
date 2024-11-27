81dae4167cd567bf0f99830a4fecd0492a33db10
GitHub HyeongJun02 <khj029956@gmail.com>
2024-11-27 22:02:48 +0900

    Radix Sorting

--- 5. Various Sorting/F_Radix_Sorting.cpp
+++ 5. Various Sorting/F_Radix_Sorting.cpp
@@ -2,15 +2,62 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 // sort
 void radixSort_LSD(vector<int>& A) {
     int n = A.size();
+    vector <int>temp;
+    // int div;
+    for (int i = 1; i <= 1000; i *= 10) {
+        // cout << "i: " << i << endl;
+        for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k < n; k++) {
+                int AC = (A.at(k) / i) % 10;
+                // cout << "AC: " << AC << endl;
+                if (AC == j) {
+                    temp.push_back(A.at(k));
+                }
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            A[k] = temp[k];
+            // cout << A[k] << " ";
+        }
+        // cout << endl << "---" << endl;
+        temp.clear();
+    }
 }
-void radixSort_MSD(vector<int>& A) {
-    int n = A.size();
+void radixSort_MSD(vector<int>& A, int tries = 0) {
+    if (tries == 0) {
+        int max = *max_element(A.begin(), A.end());
+        tries = 1;
+        while (max / tries > 0) tries *= 10;
+        tries /= 10;
+    }
+
+    if (tries == 0) return;
+
+    vector<vector<int>> nTemp(10);
+    for (int num : A) {
+        int digit = (num / tries) % 10;
+        nTemp[digit].push_back(num);
+    }
+
+    size_t index = 0;
+    for (int i = 0; i < 10; i++) {
+        if (!nTemp[i].empty()) {
+            if (tries / 10 > 0) {
+                radixSort_MSD(nTemp[i], tries / 10);
+            }
+            for (int num : nTemp[i]) {
+                A[index++] = num;
+            }
+        }
+    }
 }
 
 // read
