b2b08fd923ef60812811fb19e45ffba8f769c68b
GitHub HyeongJun02 <khj029956@gmail.com>
2024-12-09 19:35:42 +0900

    Clustering

--- 6. Approximation/E_ClusteringProblem.cpp
+++ 6. Approximation/E_ClusteringProblem.cpp
@@ -1,140 +1,100 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <fstream>
 #include <cmath>
 #include <set>
 #include <limits>
-#include <cstdlib>
-#include <ctime>
 #include <algorithm>
+
+#define CLUSTER_COUNT 8
 
 using namespace std;
 
 struct Point {
     double x, y;
-    int cluster;
-
-    Point(double x, double y) : x(x), y(y), cluster(-1) {}
+    Point(double x, double y) : x(x), y(y) {}
 };
 
-double distance(const Point& p1, const Point& p2) {
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+double distance(Point a, Point b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
-void print(const vector<Point>& points, const vector<Point>& centers, int k) {
-    for (int j = 0; j < k; ++j) {
-        cout << "Cluster" << j + 1 << endl;
-        cout << "Center: (" << centers[j].x << ", " << centers[j].y << ")\n";
-        cout << "Points: ";
-        for (const auto& point : points) {
-            if (point.cluster == j) {
-                cout << "(" << point.x << ", " << point.y << ") ";
-            }
-        }
-        cout << "\n\n";
-    }
-}
-
-void clustering(vector<Point>& points, int k) {
-    vector<Point> centers;
-
-    // 1
+void clustering(vector<Point>& points, int k, vector<vector<Point>>& clusters, vector<Point>& centers) {
+    int n = points.size();
     centers.push_back(points[0]);
 
-    // 2
+    vector<bool> isCenter(n, false);
+    isCenter[0] = true;
+
     for (int j = 1; j < k; ++j) {
-        vector<double> distances(points.size(), numeric_limits<double>::max());
+        double maxDistance = -1;
+        int next = -1;
 
-        for (size_t i = 0; i < points.size(); ++i) {
-            if (points[i].cluster != -1) continue;
-
-            for (const auto& center : centers) {
-                double dist = distance(points[i], center);
-                distances[i] = min(distances[i], dist);
-            }
-        }
-
-        int newCenterIndex = max_element(distances.begin(), distances.end()) - distances.begin();
-        centers.push_back(points[newCenterIndex]);
-        points[newCenterIndex].cluster = j;
-    }
-
-    // 3
-    for (auto& point : points) {
-        double minDistance = numeric_limits<double>::max();
-        int closestCenter = -1;
-
-        for (size_t j = 0; j < centers.size(); ++j) {
-            double dist = distance(point, centers[j]);
-            if (dist < minDistance) {
-                minDistance = dist;
-                closestCenter = j;
-            }
-        }
-        point.cluster = closestCenter;
-    }
-
-    bool changed = true;
-    while (changed) {
-        changed = false;
-
-        vector<Point> newCenters(k, Point(0, 0));
-        vector<int> clusterCounts(k, 0);
-
-        for (const auto& point : points) {
-            newCenters[point.cluster].x += point.x;
-            newCenters[point.cluster].y += point.y;
-            clusterCounts[point.cluster]++;
-        }
-
-        for (int j = 0; j < k; ++j) {
-            if (clusterCounts[j] > 0) {
-                newCenters[j].x /= clusterCounts[j];
-                newCenters[j].y /= clusterCounts[j];
-            }
-        }
-
-        for (int j = 0; j < k; ++j) {
-            if (distance(centers[j], newCenters[j]) > 1e-6) {
-                changed = true;
-            }
-        }
-        centers = newCenters;
-
-        for (auto& point : points) {
-            double minDistance = numeric_limits<double>::max();
-            int closestCenter = -1;
-
-            for (size_t j = 0; j < centers.size(); ++j) {
-                double dist = distance(point, centers[j]);
-                if (dist < minDistance) {
-                    minDistance = dist;
-                    closestCenter = j;
+        for (int i = 0; i < n; ++i) {
+            if (!isCenter[i]) {
+                double minDistance = numeric_limits<double>::max();
+                for (const auto& center : centers) {
+                    minDistance = min(minDistance, distance(points[i], center));
+                }
+                if (minDistance > maxDistance) {
+                    maxDistance = minDistance;
+                    next = i;
                 }
             }
-            point.cluster = closestCenter;
         }
+        centers.push_back(points[next]);
+        isCenter[next] = true;
     }
 
-    print(points, centers, k);
+    clusters.clear();
+    clusters.resize(k);
+
+    for (const auto& point : points) {
+        double minDistance = numeric_limits<double>::max();
+        int index = 0;
+        for (int i = 0; i < k; ++i) {
+            double dist = distance(point, centers[i]);
+            if (dist < minDistance) {
+                minDistance = dist;
+                index = i;
+            }
+        }
+        clusters[index].push_back(point);
+    }
+}
+
+void print(const vector<vector<Point>>& clusters, const vector<Point>& centers) {
+    for (size_t i = 0; i < clusters.size(); ++i) {
+        cout << "Cluster " << i + 1 << endl;
+        cout << "Center (" << centers[i].x << ", " << centers[i].y << ")" << endl;
+        cout << "-------------------------------------------" << endl;
+        for (const auto& point : clusters[i]) {
+            cout << "(" << point.x << ", " << point.y << ")" << endl;
+        }
+        cout << "===========================================" << endl;
+    }
 }
 
 int main() {
     ifstream inputFile("clustering_input.txt");
+    set<pair<int, int>> inputPoints;
+    int x, y;
+    vector<vector<Point>> clusters;
+    vector<Point> centers;
 
-    set<pair<double, double>> uniquePoints;
-    vector<Point> points;
-
-    double x, y;
     while (inputFile >> x >> y) {
-        if (uniquePoints.insert({x, y}).second) {
-            points.emplace_back(x, y);
-        }
+        inputPoints.insert({ x, y });
     }
     inputFile.close();
 
-    int k = 8;
-    clustering(points, k);
+    vector<Point> points;
+    for (const auto& p : inputPoints) {
+        points.emplace_back(p.first, p.second);
+    }
+
+    clustering(points, CLUSTER_COUNT, clusters, centers);
+
+    print(clusters, centers);
 
     return 0;
 }
